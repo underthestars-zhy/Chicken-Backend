1d60d00a3db539f6585d4422573af547b0ec2e31
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-09-16 17:00:08 -0400

    fuck

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,8 @@ const app = new Elysia()
     .use(Logestic.preset('common'))
     .get("/", () => ({status: 200}))
     .post('/translate', async ({query, headers, body}) => {
+        return {status: 200}
+
         console.log('do')
         const level = headers['ocp-apim-subscription-region'] as unknown as 1 | 2 | 3 | 4 | 5 | 6
         const hsk_vocabulary: string[] = []
